fa2ee71717dab00c578094a47f95cedb924d8de7
Марина Сулаева <sulaevamv@gmail.com>
2023-01-12 21:59:17 +0600

    Сделано ДЗ методам объектов (добавлены методы toString, equals, hashcode)

--- src/homework_objects/Book.java
+++ src/homework_objects/Book.java
@@ -1,5 +1,7 @@
 package homework_objects;
 
+import java.util.Objects;
+
 public class Book {
     private final String nameBook;
     private final Author writer;
@@ -12,19 +14,37 @@ public class Book {
     }
 
     public String getNameBook() {
-        return this.nameBook;
+        return nameBook;
     }
 
     public Author getWriter() {
-        return this.writer;
+        return writer;
     }
 
     public int getYearOfPublication() {
-        return this.yearOfPublication;
+        return yearOfPublication;
     }
 
     public void setYearOfPublication(int yearOfPublication) {
         this.yearOfPublication = yearOfPublication;
     }
 
+    @Override
+    public String toString() {
+
+        return "Название книги: '" + nameBook + "', " + writer.toString() + ", год публикации книги: " + yearOfPublication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return nameBook.equals(book.nameBook) && writer.equals(book.writer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, writer);
+    }
 }
